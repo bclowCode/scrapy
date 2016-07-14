c5ff6c99d68565a4f86ae3d33f5f0193d3bfb9cd
bclow <bclow.tw@yahoo.com.tw>
2016-07-14 16:51:54 +0800

    [02-moz-dir-persists] : set delay to 2 to simulate pause/resume

---
+++ 02-moz-dir-persists/myproject/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MyprojectItem(scrapy.Item):
+    title = scrapy.Field()
+    link = scrapy.Field()
+    image_urls = scrapy.Field()
+    imgLink = scrapy.Field()
+    desc = scrapy.Field()
+
